6660aa7817b11ec30c0caefe66cb40395fad2d7a
Maxence <maxence.poirette@epitech.eu>
2024-04-19 18:44:23 +0200

    [+] adding /help full command

--- server/src/cmd/cmd_help.c
+++ server/src/cmd/cmd_help.c
@@ -7,8 +7,44 @@
 
 #include "server.h"
 
+
+char *list_commands(void)
+{
+    char *list_commands =
+    "'/use' ?[“team_uuid”] ?[“channel_uuid”] ?[“thread_uuid”] :"
+    " Sets the command context to a team/channel/thread\n"
+    "'/create' : create a team, a channel,"
+    " a thread or a reply based on the"
+    " context you are currently in\n"
+    "'/list' : list all teams, channels, threads or replies based"
+    " on the context you are currently in\n"
+    "'/info' : display details of the users, the selected team,"
+    " the selected channel or the selected thread based on"
+    " the context you are currently in\n";
+
+    return list_commands;
+}
+
 void help_cmd(server_t *s, int client_fd)
 {
+    char *help_message =
+    "Available commands:\n"
+    "'/login' [“user_name”] : set the user_name used by client\n"
+    "'/logout' : disconnect the client from the server\n"
+    "'/users' : get the list of all users that exist on the domain\n"
+    "'/user' [“user_uuid”] : get details about the requested user\n"
+    "'/send' [“user_uuid”] [“message_body”] :"
+    " send a message to specific user\n"
+    "'/messages' [“user_uuid”] :"
+    " list all messages exchanged with the specified user\n"
+    "'/subscribe' [“team_uuid”] : subscribe to the events of a team and"
+    " its sub directories"
+    " (enable reception of all events from a team)\n"
+    "'/subscribed' ?[“team_uuid”] : list all subscribed teams "
+    " or list all users subscribed to a team\n"
+    "'/unsubscribe' [“team_uuid”] : unsubscribe from a team\n";
+
+    write(client_fd, help_message, strlen(help_message));
+    write(client_fd, list_commands(), strlen(list_commands()));
     (void)s;
-    send(client_fd, "HELP", strlen("HELP"), 0);
 }
